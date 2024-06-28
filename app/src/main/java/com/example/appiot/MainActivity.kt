05e64d3fad54bf37128e5712cc94207b060f97ae
Yazmin02 <126051524+Yazmin02@users.noreply.github.com>
2024-06-28 02:55:16 -0600

    archivos

---
+++ app/src/main/java/com/example/appiot/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.appiot
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton: Button = findViewById(R.id.start_button)
+        startButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
